e20be81cc668a6aab9e564f17d176dc5557eb666
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-22 19:50:50 +0200

    Made code more readable by putting some code in separate classes

--- DataDetectives/Helper.cs
+++ DataDetectives/Helper.cs
@@ -1,75 +1,113 @@
 ﻿namespace DataDetectives;
 using System;
 using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
-public static class Output
+public static class Helper
 {
+    public static int GetNumberOfPages()
+    {
+        int numberOfPages = 0;
+        try
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--headless");
+            options.AddArgument("--no-sandbox");
+            options.AddArgument("--disable-dev-shm-usage");
+        
+            var htmlContent1 = "";
+            using (var driver = new ChromeDriver(options))
+            {
+                string url = $"https://www.myh.se/om-oss/sok-handlingar-i-vart-diarium?katalog=Tillsynsbeslut%20yrkesh%C3%B6gskoleutbildning";
+                driver.Navigate().GoToUrl(url);
 
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+                wait.Until(d => d.FindElement(By.CssSelector("a.v-list-item")));
 
+                htmlContent1 = driver.PageSource;
+            }
+
+            var htmlDoc1 = new HtmlDocument();
+            htmlDoc1.LoadHtml(htmlContent1);
+
+            var buttons = htmlDoc1.DocumentNode.SelectNodes("//button[contains(@aria-label, 'Goto Page')]");
+        
+            numberOfPages = int.Parse(buttons[^1].InnerText.Trim());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Something went wrong: {ex}");
+        }
+
+        return numberOfPages;
+    }
+    
     public static void DisplayPages(List<string> pages)
     {
         var htmlDoc = new HtmlDocument();
-            foreach (var page in pages)
+        foreach (var page in pages)
+        {
+            htmlDoc.LoadHtml(page);
+
+            // Extrahera information från varje listobjekt (<a>-element)
+            var button = htmlDoc.DocumentNode.SelectSingleNode("//button[contains(@aria-label, 'Current Page')]");
+            if (button != null)
             {
-                htmlDoc.LoadHtml(page);
-
-                // Extrahera information från varje listobjekt (<a>-element)
-                var button = htmlDoc.DocumentNode.SelectSingleNode("//button[contains(@aria-label, 'Current Page')]");
-                if (button != null)
-                {
-                    Console.WriteLine($"Page {button.InnerText.Trim()}.");
-                }
-                else
-                {
-                    Console.WriteLine("Page number not found.");
-                }
+                Console.WriteLine($"Page {button.InnerText.Trim()}.");
+            }
+            else
+            {
+                Console.WriteLine("Page number not found.");
+            }
 
 
-                var listItemNodes = htmlDoc.DocumentNode.SelectNodes("//a[contains(@class, 'v-list-item') and contains(@class, 'v-list-item--link')]");
-                if (listItemNodes != null)
+            var listItemNodes = htmlDoc.DocumentNode.SelectNodes("//a[contains(@class, 'v-list-item') and contains(@class, 'v-list-item--link')]");
+            if (listItemNodes != null)
+            {
+                foreach (var listItem in listItemNodes)
                 {
-                    foreach (var listItem in listItemNodes)
+                    // Extrahera diarienummer
+                    var diaryNumberNode = listItem.SelectSingleNode(".//div[contains(@class, 'v-list-item__subtitle') and contains(@class, 'letter-space-2')]");
+                    if (diaryNumberNode != null)
                     {
-                        // Extrahera diarienummer
-                        var diaryNumberNode = listItem.SelectSingleNode(".//div[contains(@class, 'v-list-item__subtitle') and contains(@class, 'letter-space-2')]");
-                        if (diaryNumberNode != null)
-                        {
-                            Console.WriteLine("Diarienummer: " + diaryNumberNode.InnerText.Trim());
-                        }
-                        else
-                        {
-                            Console.WriteLine("Inget diarienummer hittades.");
-                        }
+                        Console.WriteLine("Diarienummer: " + diaryNumberNode.InnerText.Trim());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Inget diarienummer hittades.");
+                    }
 
-                        // Extrahera granskningsinformation
-                        var reviewNode = listItem.SelectSingleNode(".//div[contains(@class, 'v-list-item__title') and contains(@class, 'myh-h3')]");
-                        if (reviewNode != null)
-                        {
-                            Console.WriteLine("Granskning: " + reviewNode.InnerText.Trim());
-                        }
-                        else
-                        {
-                            Console.WriteLine("Ingen granskningsinformation hittades.");
-                        }
+                    // Extrahera granskningsinformation
+                    var reviewNode = listItem.SelectSingleNode(".//div[contains(@class, 'v-list-item__title') and contains(@class, 'myh-h3')]");
+                    if (reviewNode != null)
+                    {
+                        Console.WriteLine("Granskning: " + reviewNode.InnerText.Trim());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ingen granskningsinformation hittades.");
+                    }
 
-                        // Extrahera aktörsnamn
-                        var actorNode = listItem.SelectSingleNode(".//span[contains(@class, 'v-card') and contains(@class, 'text--primary') and contains(@class, 'myh-body-2')]");
-                        if (actorNode != null)
-                        {
-                            Console.WriteLine("Aktör: " + actorNode.InnerText.Trim() + "\n");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Ingen aktör hittades.");
-                        }
+                    // Extrahera aktörsnamn
+                    var actorNode = listItem.SelectSingleNode(".//span[contains(@class, 'v-card') and contains(@class, 'text--primary') and contains(@class, 'myh-body-2')]");
+                    if (actorNode != null)
+                    {
+                        Console.WriteLine("Aktör: " + actorNode.InnerText.Trim() + "\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ingen aktör hittades.");
                     }
                 }
-                else
-                {
-                    Console.WriteLine("Inga listobjekt hittades.");
-                }
-
-                Console.WriteLine("-----------------------------------\n\n");
             }
+            else
+            {
+                Console.WriteLine("Inga listobjekt hittades.");
+            }
+
+            Console.WriteLine("-----------------------------------\n\n");
+        }
     }
 }
--- DataDetectives/Program.cs
+++ DataDetectives/Program.cs
@@ -1,44 +1,20 @@
-﻿using System.Diagnostics;
-
-namespace DataDetectives;
+﻿namespace DataDetectives;
 using System;
+using System.Diagnostics;
 using HtmlAgilityPack;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         Stopwatch sw = Stopwatch.StartNew();
-        Stopwatch.StartNew();
         
-        var options = new ChromeOptions();
-        options.AddArgument("--headless");
-        options.AddArgument("--no-sandbox");
-        options.AddArgument("--disable-dev-shm-usage");
-        
-        var htmlContent1 = "";
-        using (var driver = new ChromeDriver(options))
-        {
-            string url = $"https://www.myh.se/om-oss/sok-handlingar-i-vart-diarium?katalog=Tillsynsbeslut%20yrkesh%C3%B6gskoleutbildning";
-            driver.Navigate().GoToUrl(url);
-
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-            wait.Until(d => d.FindElement(By.CssSelector("a.v-list-item")));
-
-            htmlContent1 = driver.PageSource;
-        }
-
-        var htmlDoc1 = new HtmlDocument();
-        htmlDoc1.LoadHtml(htmlContent1);
-
-        var buttons = htmlDoc1.DocumentNode.SelectNodes("//button[contains(@aria-label, 'Goto Page')]");
-        int numberOfPages = int.Parse(buttons[^1].InnerText.Trim());
+        int numberOfPages = Helper.GetNumberOfPages();
 
         try
         {
+            sw.Start();
+            
             List<Thread> pageParsers = new List<Thread>();
             for (int i = 0; i < 8; i++)
             {
@@ -52,85 +28,25 @@ class Program
                 thread.Join();
             }
             
-            var sortedPages = PageParser.Pages.OrderBy(pageHtml =>
-            {
-                var htmlDoc = new HtmlDocument();
-                htmlDoc.LoadHtml(pageHtml);
-                
-                var pageButton = htmlDoc.DocumentNode.SelectSingleNode("//button[@aria-current='true']");
-                return pageButton != null ? int.Parse(pageButton.InnerText.Trim()) : 0;
-            }).ToList();
-            
-            var htmlDoc = new HtmlDocument();
-            foreach (var page in sortedPages)
-            {
-                htmlDoc.LoadHtml(page);
-
-                // Extrahera information från varje listobjekt (<a>-element)
-                var button = htmlDoc.DocumentNode.SelectSingleNode("//button[contains(@aria-label, 'Current Page')]");
-                if (button != null)
-                {
-                    Console.WriteLine($"Page {button.InnerText.Trim()}.");
-                }
-                else
-                {
-                    Console.WriteLine("Page number not found.");
-                }
-
-
-                var listItemNodes = htmlDoc.DocumentNode.SelectNodes("//a[contains(@class, 'v-list-item') and contains(@class, 'v-list-item--link')]");
-                if (listItemNodes != null)
-                {
-                    foreach (var listItem in listItemNodes)
-                    {
-                        // Extrahera diarienummer
-                        var diaryNumberNode = listItem.SelectSingleNode(".//div[contains(@class, 'v-list-item__subtitle') and contains(@class, 'letter-space-2')]");
-                        if (diaryNumberNode != null)
-                        {
-                            Console.WriteLine("Diarienummer: " + diaryNumberNode.InnerText.Trim());
-                        }
-                        else
-                        {
-                            Console.WriteLine("Inget diarienummer hittades.");
-                        }
-
-                        // Extrahera granskningsinformation
-                        var reviewNode = listItem.SelectSingleNode(".//div[contains(@class, 'v-list-item__title') and contains(@class, 'myh-h3')]");
-                        if (reviewNode != null)
-                        {
-                            Console.WriteLine("Granskning: " + reviewNode.InnerText.Trim());
-                        }
-                        else
-                        {
-                            Console.WriteLine("Ingen granskningsinformation hittades.");
-                        }
-
-                        // Extrahera aktörsnamn
-                        var actorNode = listItem.SelectSingleNode(".//span[contains(@class, 'v-card') and contains(@class, 'text--primary') and contains(@class, 'myh-body-2')]");
-                        if (actorNode != null)
-                        {
-                            Console.WriteLine("Aktör: " + actorNode.InnerText.Trim() + "\n");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Ingen aktör hittades.");
-                        }
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Inga listobjekt hittades.");
-                }
-
-                Console.WriteLine("-----------------------------------\n\n");
-            }
+            sw.Stop();
         }
         catch (Exception ex)
         {
-            Console.WriteLine("Ett fel inträffade: " + ex.Message);
+            Console.WriteLine($"Something went wrong: {ex}");
         }
+        
+        var sortedPages = PageParser.Pages.OrderBy(pageHtml =>
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(pageHtml);
+                
+            var pageButton = htmlDoc.DocumentNode.SelectSingleNode("//button[@aria-current='true']");
+            return pageButton != null ? int.Parse(pageButton.InnerText.Trim()) : 0;
+        }).ToList();
+        
+        Helper.DisplayPages(sortedPages);
 
-        sw.Stop();
+        
         Console.WriteLine($"Time elapsed attempting to scrape {numberOfPages} pages: {sw.ElapsedMilliseconds} ms.");
         Console.WriteLine($"Pages scraped: {PageParser.Pages.Count}.\n");
     }
