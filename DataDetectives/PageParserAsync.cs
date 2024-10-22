eaf94df175420496d3ac381ddd1ee0b6b93a2560
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-22 16:37:13 +0200

    Added code to initiate chromeium instance

--- DataDetectives/PageParserAsync.cs
+++ DataDetectives/PageParserAsync.cs
@@ -17,6 +17,24 @@ public static class PageParserAsync
         options.AddArgument("--no-sandbox");
         options.AddArgument("--disable-dev-shm-usage");
 
+        var htmlContent1 = "";
+        using (var driver = new ChromeDriver(options))
+        {
+            string url = $"https://www.myh.se/om-oss/sok-handlingar-i-vart-diarium?katalog=Tillsynsbeslut%20yrkesh%C3%B6gskoleutbildning";
+            driver.Navigate().GoToUrl(url);
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            wait.Until(d => d.FindElement(By.CssSelector("a.v-list-item")));
+
+            htmlContent1 = driver.PageSource;
+        }
+
+        var htmlDoc1 = new HtmlDocument();
+        htmlDoc1.LoadHtml(htmlContent1);
+
+        var buttons = htmlDoc1.DocumentNode.SelectNodes("//button[contains(@aria-label, 'Goto Page')]");
+        int numberOfPages = int.Parse(buttons[buttons.Count - 1].InnerText.Trim());
+
         await Task.Run(() =>
         {
             for (int i = 1; i < numberOfPages; i++)
